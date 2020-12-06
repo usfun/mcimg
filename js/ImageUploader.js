3b8a6f832a67644abd31536260744420a5d61a83
usfun <usfun@foxmail.com>
2020-12-06 15:48:25 +0800

    1.25

--- js/ImageUploader.js
+++ js/ImageUploader.js
@@ -8,7 +8,7 @@
                 selectButtonCopy: '选择文件',
                 secondarySelectButtonCopy: '选择更多的文件',
                 dropZone: $(this),
-                fileTypeWhiteList: ['jpg', 'png', 'jpeg', 'gif', 'bmp', 'webp'],
+                fileTypeWhiteList: ['jpg', 'png', 'jpeg', 'gif', 'bmp', 'webp', 'svg'],
                 badFileTypeMessage: '对不起，我们不能接受这种类型的文件。',
                 ajaxUrl: '/ajax/upload',
                 ajaxBfun: function(r) {
